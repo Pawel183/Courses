1ddefc79b893e77ec44c06c7e19b0bbf7a3ad8d6
Paweł Łangowski <pawlan@st.amu.edu.pl>
2025-01-11 18:35:09 +0100

    Autowired Annotation

---
+++ SpringExample/src/main/java/com/autowired/annotation/Employee.java
@@ -0,0 +1,59 @@
+package com.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+
+    @Value("100")
+    private int employeeId;
+    @Value("John")
+    private String firstName;
+    @Value("Doe")
+    private String lastName;
+    @Value("1000.0")
+    private double salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee {" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}

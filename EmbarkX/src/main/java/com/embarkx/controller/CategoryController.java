967b3642bb802bf18bdfb13624b00041bd6a164d
Paweł Łangowski <pawlan@st.amu.edu.pl>
2025-01-12 19:32:02 +0100

    Setting up Category Service

--- EmbarkX/src/main/java/com/embarkx/controller/CategoryController.java
+++ EmbarkX/src/main/java/com/embarkx/controller/CategoryController.java
@@ -1,10 +1,9 @@
 package com.embarkx.controller;
 
 import com.embarkx.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.embarkx.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,16 +11,18 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
-    private final List<Category> categories = new ArrayList<>();
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/api/public/categories")
     public List<Category> getCategories() {
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/categories")
     public String addCategory(@RequestBody Category category) {
-        categories.add(category);
+        categoryService.createCategory(category);
         return "Category added successfully";
     }
+
 }
---
+++ EmbarkX/src/main/java/com/embarkx/service/CategoryService.java
@@ -0,0 +1,10 @@
+package com.embarkx.service;
+
+import com.embarkx.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+}

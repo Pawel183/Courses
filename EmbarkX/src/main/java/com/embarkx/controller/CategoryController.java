3a273b5b22d44874d3933b02eb2e8bac1852f01d
Paweł Łangowski <pawlan@st.amu.edu.pl>
2025-01-13 19:08:41 +0100

    Use ResponseEntity

--- EmbarkX/src/main/java/com/embarkx/controller/CategoryController.java
+++ EmbarkX/src/main/java/com/embarkx/controller/CategoryController.java
@@ -3,9 +3,11 @@ package com.embarkx.controller;
 import com.embarkx.model.Category;
 import com.embarkx.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -26,7 +28,12 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/public/categories/{categoryId}")
-    public String deleteCategory(@PathVariable long categoryId) {
-        return categoryService.deleteCategory(categoryId);
+    public ResponseEntity<String> deleteCategory(@PathVariable long categoryId) {
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
     }
 }
--- EmbarkX/src/main/java/com/embarkx/service/CategoryServiceImpl.java
+++ EmbarkX/src/main/java/com/embarkx/service/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.embarkx.service;
 
 import com.embarkx.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,10 +30,9 @@ public class CategoryServiceImpl implements CategoryService {
     public String deleteCategory(long categoryId) {
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId() == categoryId)
-                .findFirst().orElse(null);
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
 
-        if (category == null)
-            return "Category not found";
         categories.remove(category);
 
         return "Category with categoryId: " + categoryId + " deleted successfully !!";

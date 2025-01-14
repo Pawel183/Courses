75689b1d6af298cb73844711aa1ae0cb54713b5d
Paweł Łangowski <pawlan@st.amu.edu.pl>
2025-01-14 19:22:54 +0100

    Add CategoryRepository and modify CategoryServiceImpl

--- EmbarkX/src/main/java/com/embarkx/service/CategoryServiceImpl.java
+++ EmbarkX/src/main/java/com/embarkx/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.embarkx.service;
 
 import com.embarkx.model.Category;
+import com.embarkx.repo.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,41 +13,36 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    public List<Category> categories = new ArrayList<>();
-    private Long nextId = 1L;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId);
-        nextId++;
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream()
-                .filter(c -> c.getCategoryId() == categoryId)
-                .findFirst()
+        Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
 
-        categories.remove(category);
+        categoryRepository.delete(category);
 
         return "Category with categoryId: " + categoryId + " deleted successfully!";
     }
 
     @Override
     public String updateCategory(Long categoryId, Category category) {
-        Category categoryToUpdate = categories.stream()
-                .filter(c -> c.getCategoryId() == categoryId)
-                .findFirst()
+        Category categoryToUpdate = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
 
         categoryToUpdate.setCategoryName(category.getCategoryName());
+        categoryRepository.save(categoryToUpdate);
 
         return "Category with categoryId: " + categoryId + " updated successfully!";
     }

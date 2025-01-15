f467942a90a834964a88323daaff9bf7f1e76c47
Paweł Łangowski <pawlan@st.amu.edu.pl>
2025-01-15 13:27:24 +0100

    Use NotBlank() and Valid() annotation

--- EmbarkX/src/main/java/com/embarkx/controller/CategoryController.java
+++ EmbarkX/src/main/java/com/embarkx/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.embarkx.controller;
 
 import com.embarkx.model.Category;
 import com.embarkx.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> addCategory(@RequestBody Category category) {
+    public ResponseEntity<String> addCategory(@Valid @RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully!", HttpStatus.CREATED);
     }

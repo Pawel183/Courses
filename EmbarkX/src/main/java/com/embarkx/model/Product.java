60839df69b2692cd9fb548337811d2bfd02e9a6c
Paweł Łangowski <pawlan@st.amu.edu.pl>
2025-01-19 13:22:46 +0100

    Change strategy from AUTO to IDENTITY

--- EmbarkX/src/main/java/com/embarkx/model/Product.java
+++ EmbarkX/src/main/java/com/embarkx/model/Product.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
     private String productName;
     private String image;

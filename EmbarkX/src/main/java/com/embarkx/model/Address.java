7a3139e7588cbefb1b8129c34209799bbf5d5606
Paweł Łangowski <pawlan@st.amu.edu.pl>
2025-02-12 18:12:40 +0100

    Implementing Addresses

--- EmbarkX/src/main/java/com/embarkx/model/Address.java
+++ EmbarkX/src/main/java/com/embarkx/model/Address.java
@@ -1,20 +1,49 @@
 package com.embarkx.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
+@Table(name = "addresses")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
 
+    @NotBlank
+    @Size(min = 5, message = "Street name must be at least 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must be at least 5 characters")
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 2, message = "City name must be at least 2 characters")
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, message = "State name must be at least 2 characters")
+    private String state;
+
+    @NotBlank
+    @Size(min = 2, message = "Country name must be at least 2 characters")
+    private String country;
+
+    @NotBlank
+    @Size(min = 6, message = "Pin code name must be at least 6 characters")
+    private String pinCode;
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
 }

f15d9e46292a132f84a0fca7eb5af0fb2dfce7c4
Pawel183 <langowski.pawel183@gmail.com>
2025-03-15 09:12:32 +0100

    Managing User Profiles and Roles

---
+++ EmbarkX/src/main/java/com/embarkx/model/User.java
@@ -0,0 +1,81 @@
+package com.embarkx.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email"),
+        }
+)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    @Column(name = "username")
+    private String userName;
+
+    @Email
+    @NotBlank
+    @Size(min = 3, max = 50)
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 120)
+    @Column(name = "password")
+    private String password;
+
+    public User(String email, String password, String userName) {
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+    }
+
+    @ManyToMany(
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @ToString.Exclude
+    @OneToMany(
+            mappedBy = "user",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            orphanRemoval = true // Remove products when user is removed
+    )
+    private Set<Product> products = new HashSet<>();
+
+    @ManyToMany(
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+    )
+    @JoinTable(
+            name = "user_address",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "address_id")
+    )
+    private List<Address> addresses = new ArrayList<>();
+}

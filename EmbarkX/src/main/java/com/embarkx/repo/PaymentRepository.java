17875b33372b4fd21f6420ed7a957d48c9cfea9e
Pawel183 <langowski.pawel183@gmail.com>
2025-03-19 15:11:26 +0100

    Create repositories for Order, OrderItem and Payment

---
+++ EmbarkX/src/main/java/com/embarkx/repo/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.embarkx.repo;
+
+import com.embarkx.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}

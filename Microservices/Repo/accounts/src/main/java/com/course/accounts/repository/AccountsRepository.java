9e20a60f9fc4a736d71ac97b01b382d52e21f514
Pawel183 <langowski.pawel183@gmail.com>
2025-10-20 19:58:30 +0200

    Section 2 - finish

--- Microservices/Repo/accounts/src/main/java/com/course/accounts/repository/AccountsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.course.accounts.repository;
-
-import com.course.accounts.entity.Accounts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountsRepository extends JpaRepository<Accounts, Long> {
-}
